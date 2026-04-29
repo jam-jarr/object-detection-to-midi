fd5823e002c909701290b1860b560373509e054f
Jamie <jamie.giarratana@gmail.com>
2026-04-28 21:46:20 -0400

    Send it

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import torch
-import cv2
-import numpy as np
-import mido
 import argparse
 import json
+from collections import defaultdict
+
+import cv2
+import mido
+import numpy as np
+import torch
 from ultralytics import YOLO
 
 VELOCITY_BASE = 80
 MIDI_CHANNEL = 0
+VELOCITY_ADD = 20
 
 DEFAULT_MIDI = {
-    "note": 60,
+    "notes": [60],
     "color": (0, 255, 0),
 }
 
@@ -22,7 +25,7 @@ class MidiController:
         self.use_file_mode = False
         self.midi_file = None
         self.track = None
-        self.active_notes = {}
+        self.active_notes = set()
 
     def open_port(self, port=None):
         available_ports = mido.get_output_names()
@@ -68,6 +71,7 @@ class MidiController:
             self.port.send(
                 mido.Message("note_on", note=note, velocity=velocity, channel=channel)
             )
+        self.active_notes.add(note)
 
     def send_note_off(self, note, velocity=0, channel=MIDI_CHANNEL):
         if self.use_file_mode and self.track:
@@ -84,9 +88,10 @@ class MidiController:
             self.port.send(
                 mido.Message("note_off", note=note, velocity=velocity, channel=channel)
             )
+        self.active_notes.remove(note)
 
     def close(self):
-        for _class_name, note in list(self.active_notes.items()):
+        for note in list(self.active_notes):
             self.send_note_off(note)
 
         if self.use_file_mode and self.midi_file:
@@ -97,6 +102,11 @@ class MidiController:
         elif self.port:
             self.port.close()
             print("MIDI port closed")
+
+
+def get_note_list(class_name):
+    class_conf = configuration.get(class_name, DEFAULT_MIDI)
+    return class_conf.get("notes")
 
 
 # Arguments
@@ -155,7 +165,7 @@ if args.debug:
     print(vars(args))
     print("-" * 50)
 
-# COCO class to MIDI note mapping
+# load configuration file
 with open("configuration.json") as f:
     configuration = json.load(f)
 
@@ -174,7 +184,8 @@ model = torch.hub.load(
 model.conf = args.conf
 model.iou = args.iou
 
-model.cuda()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 # Initialize webcam
 cap = cv2.VideoCapture(args.device)
@@ -188,11 +199,19 @@ midi.open_port(args.port)
 
 print("Webcam opened. Press 'q' to quit.")
 
-active_detections = set()
-
 cv2.namedWindow("YOLOv5 Detections", cv2.WINDOW_NORMAL)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+available_classes = set()
+
+# for class_name, class_conf in configuration.items():
+#     if not class_conf.get("disabled", False):
+#         available_classes.add(class_name)
+
+available_classes = {"person"}
+
+playing_notes = defaultdict(dict)
 
 try:
     while True:
@@ -209,9 +228,13 @@ try:
             print(f"Frame size: {frame.shape}")
 
         detections = results.pandas().xyxy[0]
-        print(results.pandas().xyxy[0])
 
-        current_detections = set()
+        # Remove detections that are not in the available classes from the pandas dataframe
+        detections_names = set(detections["name"])
+        available_detections = detections_names & available_classes
+        detections = detections[detections["name"].isin(available_detections)]
+
+        current_detections = defaultdict(int)
 
         for _, row in detections.iterrows():
             class_name = row["name"]
@@ -251,23 +274,58 @@ try:
                 2,
             )
 
-            current_detections.add(class_name)
-            if class_name not in active_detections:
-                midi_note = class_conf.get("note")
-                midi.send_note_on(midi_note, VELOCITY_BASE)
-                midi.active_notes[class_name] = midi_note
-                print(f"note_on: {class_name} -> MIDI {midi_note}, vel {VELOCITY_BASE}")
+            current_detections[class_name] += 1
 
-            active_detections.add(class_name)
+        # After the number of detections for a class is known, play the corresponding MIDI notes
 
-        removed = active_detections - current_detections
-        for class_name in removed:
-            midi_note = class_conf.get("note")
-            midi.send_note_off(midi_note)
-            if class_name in midi.active_notes:
-                del midi.active_notes[class_name]
-            print(f"note_off: {class_name} -> MIDI {midi_note}")
-            active_detections.discard(class_name)
+        if args.debug:
+            print(f"Current detections: {current_detections}")
+
+        notes_to_play = defaultdict(dict)
+
+        # Set the MIDI notes to play
+        # More detections means more notes
+        for class_name, count in current_detections.items():
+            for _ in range(count):
+                blacklist = set(notes_to_play)
+                for note in playing_notes[class_name].items():
+                    blacklist.add(note)
+                note_list = get_note_list(class_name)
+                available = set(note_list) - blacklist
+
+                # If the number of detections eceeds the number of available notes,
+                # play another note in the list with a higher velocity
+                if not available:
+                    mult = count // len(note_list)
+                    note = np.random.choice(note_list)
+                    notes_to_play[class_name][note] = ()
+                    VELOCITY_BASE + VELOCITY_ADD * mult
+                else:
+                    note = np.random.choice(list(available))
+                    notes_to_play[class_name][note] = VELOCITY_BASE
+
+        if args.debug:
+            print(f"Notes wanting to play: {notes_to_play}")
+
+        # Turn off notes that are no longer active for a class
+        for class_name, notes in playing_notes.items():
+            for note, vel in notes.items():
+                if note not in notes_to_play[class_name]:
+                    midi.send_note_off(note)
+                    if args.debug:
+                        print(f"note_off: {class_name} -> MIDI {note}")
+
+        # Turn on notes
+        for class_name, notes in notes_to_play.items():
+            for note, vel in notes.items():
+                midi.send_note_on(note, vel)
+                if args.debug:
+                    print(f"note_on: {class_name} -> MIDI {note}, vel {VELOCITY_BASE}")
+
+        playing_notes = notes_to_play
+
+        if args.debug:
+            print(f"Notes playing: {midi.active_notes}")
 
         cv2.imshow("YOLOv5 Detections", original_frame)
 
